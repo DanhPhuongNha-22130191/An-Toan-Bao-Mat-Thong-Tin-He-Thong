ce59441ab89642c068cc40fc120b4cce52d15cf8
hien <22130080@st.hcmuaf.edu.vn>
2025-02-23 19:43:34 +0700

    feat(cart): CartService và update cart

---
+++ ATBM/src/main/java/controllers/CartController.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.CartDTO;
+import models.CartItem;
+import services.CartService;
+
+public class CartController extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String action = req.getParameter("action");
+		if ("update".equals(action.toLowerCase())) {
+			updateCart(req, resp);
+		}
+	}
+
+	private void updateCart(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String[] productQuantity = req.getParameterValues("product-quantity");
+		if (productQuantity != null) {
+			CartService service = new CartService();
+			long accountId = Long.parseLong(req.getAttribute("accountId").toString());
+			for (String string : productQuantity) {
+				StringTokenizer tokens = new StringTokenizer(string, "-");
+				long productId = Long.parseLong(tokens.nextToken());
+				int quantity = Integer.parseInt(tokens.nextToken());
+				service.update(new CartItem(accountId, productId, quantity));
+			}
+		}
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		CartService service = new CartService();
+		long accountId = Long.parseLong(req.getAttribute("accountId").toString());
+		CartDTO dto = service.convertToDTO(accountId);
+		req.setAttribute("cartDTO", dto);
+		req.getRequestDispatcher("").forward(req, resp);
+	}
+
+}
---
+++ ATBM/src/main/java/dto/CartDTO.java
@@ -0,0 +1,91 @@
+package dto;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import models.Product;
+
+public class CartDTO {
+	private List<CartItemDTO> items;
+
+	public CartDTO() {
+		items = new LinkedList<CartDTO.CartItemDTO>();
+	}
+	public void add(Product product, int quantity) {
+		items.add(new CartItemDTO(product.getProductId(), product.getName(), product.getImage(), product.getPrice(),
+				quantity));
+	}
+
+	public List<CartItemDTO> getItems() {
+		return items;
+	}
+
+	public void addAll(List<CartItemDTO> listItems) {
+		items.addAll(listItems);
+	}
+
+	public double getSubTotal() {
+		return items.stream().mapToDouble(e -> e.getTotalPrice()).sum();
+	}
+
+	public class CartItemDTO {
+		private long productId;
+		private String productName;
+		private String productImg;
+		private double productPrice;
+		private int quantity;
+
+		public CartItemDTO(long productId, String productName, String productImg, double productPrice, int quantity) {
+			this.productId = productId;
+			this.productName = productName;
+			this.productImg = productImg;
+			this.productPrice = productPrice;
+			this.quantity = quantity;
+		}
+
+		public double getTotalPrice() {
+			return quantity * productPrice;
+		}
+
+		public long getProductId() {
+			return productId;
+		}
+
+		public void setProductId(long productId) {
+			this.productId = productId;
+		}
+
+		public String getProductName() {
+			return productName;
+		}
+
+		public void setProductName(String productName) {
+			this.productName = productName;
+		}
+
+		public String getProductImg() {
+			return productImg;
+		}
+
+		public void setProductImg(String productImg) {
+			this.productImg = productImg;
+		}
+
+		public double getProductPrice() {
+			return productPrice;
+		}
+
+		public void setProductPrice(double productPrice) {
+			this.productPrice = productPrice;
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+
+	}
+}
