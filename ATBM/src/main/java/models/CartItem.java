f77afa0a3af4d7320e86c2d8dbe9baec21323752
hien <22130080@st.hcmuaf.edu.vn>
2025-03-09 13:08:28 +0700

    wip(class): lấy class cho nhánh checkout

---
+++ ATBM/src/main/java/models/CartItem.java
@@ -0,0 +1,50 @@
+package models;
+/**
+ * @author minhhien
+ * Thể hiện sản phẩm trong giỏ hàng của người dùng
+ */
+public class CartItem {
+	private long accountId;
+	private long productId;
+	private long orderId;
+	private int quantity;
+
+	public CartItem(long accountId, long productId, int quantity) {
+		this.accountId = accountId;
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+
+	public long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(long accountId) {
+		this.accountId = accountId;
+	}
+
+	public long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+
+}
---
+++ ATBM/src/main/java/services/CartService.java
@@ -0,0 +1,64 @@
+package services;
+
+import java.util.List;
+
+import dao.CartDao;
+import dto.CartDTO;
+import models.CartItem;
+import models.Product;
+
+public class CartService implements IService<CartItem, Long[]> {
+	private CartDao dao;
+
+	public CartService() {
+		dao = new CartDao();
+	}
+
+	@Override
+	public boolean insert(CartItem entity) {
+		if (getById(new Long[] { entity.getAccountId(), entity.getProductId() }) != null) {
+			return update(entity);
+		}
+		return dao.insert(entity);
+	}
+
+	@Override
+	public CartItem getById(Long[] id) {
+		return dao.getById(id);
+	}
+
+	@Override
+	public List<CartItem> getAll() {
+		return dao.getAll();
+	}
+
+	public CartDTO convertToDTO(long accountId) {
+		CartDTO dto = new CartDTO();
+		List<CartItem> cartItems = getByAccId(accountId);
+		ProductService productService = new ProductService();
+		for (CartItem cartItem : cartItems) {
+			Product product = productService.getById(cartItem.getProductId());
+			dto.add(product, cartItem.getQuantity());
+		}
+		return dto;
+	}
+
+	public List<CartItem> getByAccId(long accountId) {
+		return dao.getCartByAcc(accountId);
+	}
+	@Override
+	public boolean update(CartItem cartItem) {
+		Long[] id = new Long[] { cartItem.getAccountId(), cartItem.getProductId() };
+		if (cartItem.getQuantity() == 0)
+			return delete(id);
+		else
+			return dao.update(cartItem);
+	}
+
+	@Override
+	public boolean delete(Long[] id) {
+		return dao.delete(id);
+
+	}
+
+}
