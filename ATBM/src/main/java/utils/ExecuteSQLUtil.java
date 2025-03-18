d024cd30da2bb1b6d9eb4ef69f9d2b08eec186a3
LYTDUNG <22130054@st.hcmuaf.edu.vn>
2025-03-18 23:31:20 +0700

    feat : chức năng add và chi tiết sản phẩm

---
+++ ATBM/src/main/java/utils/ExecuteSQLUtil.java
@@ -0,0 +1,53 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import database.DBConnection;
+
+/**
+ * @author minhhien
+ */
+public class ExecuteSQLUtil {
+	/**
+	 * Phương thức thực hiện lệnh update, insert, delete
+	 * 
+	 * @param query lệnh query
+	 * @param data  tham số
+	 * @return true nếu thành công, ngược lại false
+	 */
+	public static boolean executeUpdate(String query, Object... data) {
+		Connection connect = DBConnection.getConnection();
+		try {
+			PreparedStatement preStatement = connect.prepareStatement(query);
+			for (int i = 0; i < data.length; i++)
+				preStatement.setObject(i + 1, data[i]);
+			return preStatement.executeUpdate() > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Phương thức thực hiện lệnh excuteQuery
+	 * 
+	 * @param query lệnh query
+	 * @param data  tham số
+	 * @return ResultSet đại diện cho từng dòng giá trị
+	 */
+	public static ResultSet ExecuteQuery(String query, Object... data) {
+		Connection connect = DBConnection.getConnection();
+		try {
+			PreparedStatement preStatement = connect.prepareStatement(query);
+			for (int i = 0; i < data.length; i++)
+				preStatement.setObject(i + 1, data[i]);
+			return preStatement.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
