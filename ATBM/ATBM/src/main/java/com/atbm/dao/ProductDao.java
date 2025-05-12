1ee35adb3b005a567bf019692d67a7e4c4b63720
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2025-05-12 13:00:45 +0700

    Chỉnh sửa file query.sql và ProductDao

--- ATBM/ATBM/src/main/java/com/atbm/dao/ProductDao.java
+++ ATBM/ATBM/src/main/java/com/atbm/dao/ProductDao.java
@@ -1,6 +1,5 @@
 package com.atbm.dao;
 
-
 import com.atbm.dto.Brand;
 import com.atbm.dto.Strap;
 import com.atbm.models.Product;
@@ -15,20 +14,20 @@ public class ProductDao implements IDao<Product, Long> {
 
 	// Thêm sản phẩm mới
 	@Override
-	public boolean insert(Product Product) {
-		String query = "INSERT INTO Product (name, price, description, stock, image, haveTrending, size, waterResistance, batteryLife) "
-				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		return ExecuteSQLUtil.executeUpdate(query, Product.getName(), Product.getPrice(), Product.getDescription(),
-				Product.getStock(), Product.getImage(), Product.isHaveTrending(), Product.getSize(),
-				Product.isWaterResistance());
+	public boolean insert(Product product) {
+		String query = "INSERT INTO Product (name, price, description, stock, image, haveTrending, size, waterResistance) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		return ExecuteSQLUtil.executeUpdate(query, product.getName(), product.getPrice(), product.getDescription(),
+				product.getStock(), product.getImage(), product.isHaveTrending(), product.getSize(),
+				product.isWaterResistance());
 	}
 
 	// Cập nhật thông tin sản phẩm
-	public boolean update(Product Product) {
-		String query = "UPDATE Product SET name = ?, price = ?, description = ?, stock = ?, image = ?, haveTrending = ?, size = ?, waterResistance = ?, batteryLife = ? WHERE ProductId = ?";
-		return ExecuteSQLUtil.executeUpdate(query, Product.getName(), Product.getPrice(), Product.getDescription(),
-				Product.getStock(), Product.getImage(), Product.isHaveTrending(), Product.getSize(),
-				Product.isWaterResistance(), Product.getProductId());
+	public boolean update(Product product) {
+		String query = "UPDATE Product SET name = ?, price = ?, description = ?, stock = ?, image = ?, haveTrending = ?, size = ?, waterResistance = ? WHERE ProductId = ?";
+		return ExecuteSQLUtil.executeUpdate(query, product.getName(), product.getPrice(), product.getDescription(),
+				product.getStock(), product.getImage(), product.isHaveTrending(), product.getSize(),
+				product.isWaterResistance(), product.getProductId());
 	}
 
 	// Xóa sản phẩm theo ID
@@ -45,10 +44,17 @@ public class ProductDao implements IDao<Product, Long> {
 		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query, id);
 		try {
 			if (rs != null && rs.next()) {
-				return new Product(rs.getLong("ProductId"), rs.getString("name"), rs.getDouble("price"),
-						rs.getString("description"), rs.getInt("stock"), rs.getString("image"),
-						rs.getBoolean("haveTrending"), rs.getDouble("size"), rs.getBoolean("waterResistance"),
-						rs.getDouble("batteryLife"));
+				return new Product(
+						rs.getLong("ProductId"),
+						rs.getString("name"),
+						rs.getDouble("price"),
+						rs.getString("description"),
+						rs.getInt("stock"),
+						rs.getString("image"),
+						rs.getBoolean("haveTrending"),
+						rs.getDouble("size"),
+						rs.getBoolean("waterResistance")
+				);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -59,20 +65,27 @@ public class ProductDao implements IDao<Product, Long> {
 	// Lấy tất cả sản phẩm
 	@Override
 	public List<Product> getAll() {
-		List<Product> Products = new ArrayList<>();
+		List<Product> products = new ArrayList<>();
 		String query = "SELECT * FROM Product";
 		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query);
 		try {
 			while (rs != null && rs.next()) {
-				Products.add(new Product(rs.getLong("ProductId"), rs.getString("name"), rs.getDouble("price"),
-						rs.getString("description"), rs.getInt("stock"), rs.getString("image"),
-						rs.getBoolean("haveTrending"), rs.getDouble("size"), rs.getBoolean("waterResistance"),
-						rs.getDouble("batteryLife")));
+				products.add(new Product(
+						rs.getLong("ProductId"),
+						rs.getString("name"),
+						rs.getDouble("price"),
+						rs.getString("description"),
+						rs.getInt("stock"),
+						rs.getString("image"),
+						rs.getBoolean("haveTrending"),
+						rs.getDouble("size"),
+						rs.getBoolean("waterResistance")
+				));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return Products;
+		return products;
 	}
 
 	// Lấy danh sách sản phẩm trending
@@ -82,10 +95,17 @@ public class ProductDao implements IDao<Product, Long> {
 		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query);
 		try {
 			while (rs != null && rs.next()) {
-				trendingProducts.add(new Product(rs.getLong("ProductId"), rs.getString("name"), rs.getDouble("price"),
-						rs.getString("description"), rs.getInt("stock"), rs.getString("image"),
-						rs.getBoolean("haveTrending"), rs.getDouble("size"), rs.getBoolean("waterResistance"),
-						rs.getDouble("batteryLife")));
+				trendingProducts.add(new Product(
+						rs.getLong("ProductId"),
+						rs.getString("name"),
+						rs.getDouble("price"),
+						rs.getString("description"),
+						rs.getInt("stock"),
+						rs.getString("image"),
+						rs.getBoolean("haveTrending"),
+						rs.getDouble("size"),
+						rs.getBoolean("waterResistance")
+				));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -93,7 +113,7 @@ public class ProductDao implements IDao<Product, Long> {
 		return trendingProducts;
 	}
 
-	// Lấy ds các nhãn hàng
+	// Lấy danh sách nhãn hàng
 	public List<Brand> getBrands() {
 		List<Brand> brands = new ArrayList<>();
 		String query = "SELECT * FROM Brand";
@@ -108,42 +128,54 @@ public class ProductDao implements IDao<Product, Long> {
 		return brands;
 	}
 
-	// Lấy ds sản phẩm bằng brandId
+	// Lấy sản phẩm theo brandId
 	public List<Product> getProductsByBrand(String brandId) {
-		List<Product> Products = new ArrayList<>();
-		String query = "SELECT p.* FROM Product p " + "JOIN Brand b ON p.ProductId = b.ProductId "
-				+ "WHERE b.brandId = ?";
+		List<Product> products = new ArrayList<>();
+		String query = "SELECT p.* FROM Product p JOIN Brand b ON p.ProductId = b.ProductId WHERE b.brandId = ?";
 		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query, brandId);
 		try {
 			while (rs != null && rs.next()) {
-				Products.add(new Product(rs.getLong("ProductId"), rs.getString("name"), rs.getDouble("price"),
-						rs.getString("description"), rs.getInt("stock"), rs.getString("image"),
-						rs.getBoolean("haveTrending"), rs.getDouble("size"), rs.getBoolean("waterResistance"),
-						rs.getDouble("batteryLife")));
+				products.add(new Product(
+						rs.getLong("ProductId"),
+						rs.getString("name"),
+						rs.getDouble("price"),
+						rs.getString("description"),
+						rs.getInt("stock"),
+						rs.getString("image"),
+						rs.getBoolean("haveTrending"),
+						rs.getDouble("size"),
+						rs.getBoolean("waterResistance")
+				));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return Products;
+		return products;
 	}
 
-	// Lấy ds sản phẩm bằng id dây đồng hồ
+	// Lấy sản phẩm theo strapId
 	public List<Product> getProductByStrap(String strapId) {
-		List<Product> Products = new ArrayList<>();
-		String query = "SELECT p.* FROM Product p " + "JOIN Strap s ON p.ProductId = s.ProductId "
-				+ "WHERE s.strapId = ?";
+		List<Product> products = new ArrayList<>();
+		String query = "SELECT p.* FROM Product p JOIN Strap s ON p.ProductId = s.ProductId WHERE s.strapId = ?";
 		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query, strapId);
 		try {
 			while (rs != null && rs.next()) {
-				Products.add(new Product(rs.getLong("ProductId"), rs.getString("name"), rs.getDouble("price"),
-						rs.getString("description"), rs.getInt("stock"), rs.getString("image"),
-						rs.getBoolean("haveTrending"), rs.getDouble("size"), rs.getBoolean("waterResistance"),
-						rs.getDouble("batteryLife")));
+				products.add(new Product(
+						rs.getLong("ProductId"),
+						rs.getString("name"),
+						rs.getDouble("price"),
+						rs.getString("description"),
+						rs.getInt("stock"),
+						rs.getString("image"),
+						rs.getBoolean("haveTrending"),
+						rs.getDouble("size"),
+						rs.getBoolean("waterResistance")
+				));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return Products;
+		return products;
 	}
 
 	// Lấy danh sách dây đồng hồ
@@ -153,42 +185,46 @@ public class ProductDao implements IDao<Product, Long> {
 		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query);
 		try {
 			while (rs != null && rs.next()) {
-				straps.add(new Strap(rs.getLong("ProductId"), rs.getLong("strapId"), rs.getString("color"),
-						rs.getString("material"), rs.getDouble("length")));
+				straps.add(new Strap(
+						rs.getLong("ProductId"),
+						rs.getLong("strapId"),
+						rs.getString("color"),
+						rs.getString("material"),
+						rs.getDouble("length")
+				));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return straps;
 	}
-	
 
+	// Lấy sản phẩm theo brandId (int)
 	public List<Product> getProductsByBrandId(int brandId) {
-	    List<Product> Products = new ArrayList<>();
-	    String query = "SELECT p.* FROM Product p " +
-	                   "JOIN Brand b ON p.ProductId = b.ProductId " +
-	                   "WHERE b.brandId = ?";
-	    ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query, brandId);
-	    try {
-	        while (rs != null && rs.next()) {
-	            Products.add(new Product(
-	                rs.getLong("ProductId"),
-	                rs.getString("name"),
-	                rs.getDouble("price"),
-	                rs.getString("description"),
-	                rs.getInt("stock"),
-	                rs.getString("image"),
-	                rs.getBoolean("haveTrending"),
-	                rs.getDouble("size"),
-	                rs.getBoolean("waterResistance"),
-	                rs.getDouble("batteryLife")
-	            ));
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return Products;
+		List<Product> products = new ArrayList<>();
+		String query = "SELECT p.* FROM Product p JOIN Brand b ON p.ProductId = b.ProductId WHERE b.brandId = ?";
+		ResultSet rs = ExecuteSQLUtil.ExecuteQuery(query, brandId);
+		try {
+			while (rs != null && rs.next()) {
+				products.add(new Product(
+						rs.getLong("ProductId"),
+						rs.getString("name"),
+						rs.getDouble("price"),
+						rs.getString("description"),
+						rs.getInt("stock"),
+						rs.getString("image"),
+						rs.getBoolean("haveTrending"),
+						rs.getDouble("size"),
+						rs.getBoolean("waterResistance")
+				));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return products;
 	}
+
+	// Lọc sản phẩm theo brandId, strapId, minPrice, maxPrice
 	public List<Product> filterProducts(Integer brandId, Integer strapId, Double minPrice, Double maxPrice) {
 		List<Product> products = new ArrayList<>();
 		StringBuilder query = new StringBuilder(
@@ -232,8 +268,7 @@ public class ProductDao implements IDao<Product, Long> {
 						rs.getString("image"),
 						rs.getBoolean("haveTrending"),
 						rs.getDouble("size"),
-						rs.getBoolean("waterResistance"),
-						rs.getDouble("batteryLife") // Ensure this field is in the SELECT
+						rs.getBoolean("waterResistance")
 				));
 			}
 		} catch (SQLException e) {
@@ -247,5 +282,4 @@ public class ProductDao implements IDao<Product, Long> {
 		ProductDao dao = new ProductDao();
 		System.out.println(dao.getProductsByBrandId(1));
 	}
-
 }
--- ATBM/ATBM/src/main/java/com/atbm/dto/WatchType.java
+++ ATBM/ATBM/src/main/java/com/atbm/dto/WatchType.java
@@ -1,4 +1,4 @@
-package dto;
+package com.atbm.dto;
 
 public class WatchType {
 	private long productId; 
