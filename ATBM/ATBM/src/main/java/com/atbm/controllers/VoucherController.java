04ca812ecd2377f92775a3b295f0ee16db3757b7
hien <22130080@st.hcmuaf.edu.vn>
2025-05-16 09:13:03 +0700

    feat: thêm AuthFilter và quản lý người dùng qua session
    - Áp dụng AuthFilter cho /user/* để kiểm tra đăng nhập
    - Close #2

--- ATBM/ATBM/src/main/java/com/atbm/controllers/VoucherController.java
+++ ATBM/ATBM/src/main/java/com/atbm/controllers/VoucherController.java
@@ -13,22 +13,22 @@ import java.io.IOException;
 
 @WebServlet("/user/voucher")
 public class VoucherController extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// Đặt kiểu phản hồi là text/plain để tránh lỗi hiển thị
-		resp.setContentType("text/plain");
-		resp.setCharacterEncoding("UTF-8");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Đặt kiểu phản hồi là text/plain để tránh lỗi hiển thị
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
 
-		String code = req.getParameter("voucher-code");
-		long accountId = 101; // Giả lập accountId
-
-		VoucherService service = new VoucherService();
-		if (service.applyVoucher(code, accountId)) {
-			req.getSession().setAttribute("voucher", code);
-			resp.getWriter().write("1"); // Gửi về chuỗi "1"
-		} else {
-			resp.getWriter().write("0"); // Gửi về chuỗi "0"
-		}
-	}
+        String code = req.getParameter("voucher-code");
+        dto.AccountDTO accountDTO = (dto.AccountDTO) req.getSession().getAttribute("user");
+        long accountId = accountDTO.getAccountId();
+        VoucherService service = new VoucherService();
+        if (service.applyVoucher(code, accountId)) {
+            req.getSession().setAttribute("voucher", code);
+            resp.getWriter().write("1"); // Gửi về chuỗi "1"
+        } else {
+            resp.getWriter().write("0"); // Gửi về chuỗi "0"
+        }
+    }
 }
 
