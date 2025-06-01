baf2ccd960e50834d2e86276f6d2a2524f230cfb
LYTUANDUNG <144934062+LYTUANDUNG@users.noreply.github.com>
2025-06-01 22:02:51 +0700

    Hoàn thành reCAPTCHA, mã hóa BCrypt và thêm publicKeyActive (#17)
    ]

--- ATBM/ATBM/src/main/java/com/atbm/dto/AccountDTO.java
+++ ATBM/ATBM/src/main/java/com/atbm/dto/AccountDTO.java
@@ -1,9 +1,10 @@
 package com.atbm.dto;
 
 public class AccountDTO {
-    private long accountId; 
+    private long accountId;
     private String username;
     private String email;
+    private String publicKeyActive;
 
     public AccountDTO(long accountId, String username, String email) {
         this.accountId = accountId;
@@ -11,27 +12,19 @@ public class AccountDTO {
         this.email = email;
     }
 
-    public long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(long accountId) {
+    public AccountDTO(long accountId, String username, String email, String publicKeyActive) {
         this.accountId = accountId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
+        this.publicKeyActive = publicKeyActive;
     }
+
+    public long getAccountId() { return accountId; }
+    public void setAccountId(long accountId) { this.accountId = accountId; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPublicKeyActive() { return publicKeyActive; }
+    public void setPublicKeyActive(String publicKeyActive) { this.publicKeyActive = publicKeyActive; }
 }
--- ATBM/ATBM/src/main/java/com/atbm/models/Account.java
+++ ATBM/ATBM/src/main/java/com/atbm/models/Account.java
@@ -1,31 +1,40 @@
 package com.atbm.models;
-
 import java.util.List;
 
 public class Account {
-    private long accountId;
-    private String username;
-    private String password;
-    private String email;
-    private List<String> publicKeyList;
+	private long accountId;
+	private String username;
+	private String password;
+	private String email;
     private String publicKeyActive;
 
-    public Account(long accountId, String username, String password, String email) {
-        super();
+    public Account() {}
+
+	public Account(long accountId, String username, String password, String email, String publicKeyActive) {
+		this.accountId = accountId;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.publicKeyActive = publicKeyActive;
+	}
+    public Account(long accountId,String username, String password) {
         this.accountId = accountId;
         this.username = username;
         this.password = password;
+
+    }
+    public Account(String username, String password, String email, String publicKeyActive) {
+        this.username = username;
+        this.password = password;
         this.email = email;
-    }
-
-    public String getPublicKeyActive() {
-        return publicKeyActive;
-    }
-
-    public void setPublicKeyActive(String publicKeyActive) {
         this.publicKeyActive = publicKeyActive;
     }
+    public Account(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
 
+    }
     public long getAccountId() {
         return accountId;
     }
@@ -58,4 +67,8 @@ public class Account {
         this.email = email;
     }
 
+    public String getPublicKeyActive() { return publicKeyActive; }
+    public void setPublicKeyActive(String publicKeyActive) {
+        this.publicKeyActive = publicKeyActive;
+    }
 }
